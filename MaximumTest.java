ef471398eedfbfd5b04e230fc04d8a4b2de1da35
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-14 16:45:21 +0530

    Refactored: MaximumIntegerNumber

--- MaximumTest.java
+++ MaximumTest.java
@@ -3,17 +3,16 @@ package genericsMethod;
 public class MaximumTest {
 	public static Integer maximumTest(Integer x, Integer y, Integer z) {
 		Integer max = x;
-		if(y.compareTo(max) > 0);
-		max = y;
-		if(z.compareTo(max) > 0);
-		max = z;
-		
+		if (y.compareTo(max) > 0)
+			max = y;
+		if (z.compareTo(max) > 0)
+			max = z;
 		return max;
-		
 	}
+
 	public static void main(String[] args) {
 		Integer maximum = MaximumTest.maximumTest(112, 145, 789);
-		System.out.println("Maximum Number:"+maximum);
+		System.out.println("Maximum Number:" + maximum);
 	}
 
 }
