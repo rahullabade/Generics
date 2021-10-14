d34cf5e1ea6a42409afdfedc5414dcaf825653a7
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-14 16:35:10 +0530

    Added: MaximumIntegerNumber

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,8 +1,19 @@
 package genericsMethod;
 
 public class MaximumTest {
+	public static Integer maximumTest(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if(y.compareTo(max) > 0);
+		max = y;
+		if(z.compareTo(max) > 0);
+		max = z;
+		
+		return max;
+		
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcom to Generics");
+		Integer maximum = MaximumTest.maximumTest(112, 145, 789);
+		System.out.println("Maximum Number:"+maximum);
 	}
 
 }
