f1e5231edd5018315316f57129a94eb968f98d27
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-15 22:19:52 +0530

    Added: RefactorTwo

---
+++ RefactorTwo.java
@@ -0,0 +1,31 @@
+package genericsMethod;
+
+public class RefactorTwo<T extends Comparable<T>> {
+
+	T x, y, z;
+
+	public RefactorTwo(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T maximum() {
+		return RefactorTwo.maximum(x, y, z);
+	}
+
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+		T max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.printf("The Maximum Integer Value of %d,%d,%d id %d \n ", 5, 6, 7, maximum(5, 6, 7));
+	}
+}
